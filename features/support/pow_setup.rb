617ea13630c849af84d0727ee70515ece56b5079
Brian Bommarito <bbommarito@me.com>
2012-07-02 05:35:44 -0400

    Setting up testing with pow.

---
+++ features/support/pow_setup.rb
@@ -0,0 +1,23 @@
+Capybara.default_driver = :selenium
+pow_config = "#{Rails.root}/.powenv"
+pow_config_stash = "#{Rails.root}/.powenv_original"
+
+Before do
+  Capybara.app_host = 'http://www.redtix.dev'
+
+  File.rename(pow_config, pow_config_stash) if File.exists?(pow_config)
+
+  f = File.new("#{Rails.root}/.powenv", 'w')
+  f.write('export RAILS_ENV=test')
+  f.close
+
+  FileUtils.touch("#{Rails.root}/tmp/restart.txt")
+end
+
+After do
+  File.delete(pow_config)
+
+  File.rename(pow_config_stash, pow_config) if File.exists?(pow_config_stash)
+
+  FileUtils.touch("#{Rails.root}/tmp/restart.txt")
+end
